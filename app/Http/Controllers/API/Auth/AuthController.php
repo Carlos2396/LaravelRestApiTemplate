24aa9d89033b5c506ceb774d5946a008487d0273
Carlos2396 <carlosaam96@gmail.com>
2018-12-08 14:44:16 -0600

    Updated login method to check account verification

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -11,19 +11,39 @@ use Validator;
 class AuthController extends Controller
 {
     public function login(Request $request) {
+        // validates request data, returns errors if fails
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+        
+        if($validator->fails()) {
+            return ResponseHelper::validationErrorResponse($validator->errors());
+        }
 
+        // set session if credentials are valid
         if(Auth::attempt($request->only('email', 'password'))) { 
             $user = Auth::user();
-            $token =  $user->createToken('MyApp')->accessToken;
-             
+
+            // fail if user account is not confirmed
+            if($user->email_verified_at == null) {
+                return response()->json(['message'=>'Account not confirmed.'], 418);
+            }
+
+            // Creates access token and send in response
+            $token =  $user->createToken('MyApp');
+            $tokenStr = $token->accessToken; 
+            $expiration = $token->token->expires_at->diffInSeconds(Carbon::now());
+
             return response()->json([
                 'user' => $user,
-                'token' => $token
+                'token' => $tokenStr,
+                'expiration_time' => $expiration
             ], 200); 
         } 
         else{ 
             return response()->json(['message'=>'Bad credentials'], 401); 
-        } 
+        }  
     }
 
     public function logout() {
